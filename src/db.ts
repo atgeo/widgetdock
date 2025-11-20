5d15710ef4e7a6a06d98874672822c2dca7106f3
Georges Kmeid <georgesk117@gmail.com>
2025-11-20 19:04:11 +0700

    cleanup

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,9 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-import { Client } from 'pg'
+import {drizzle} from 'drizzle-orm/node-postgres'
+import {Client} from 'pg'
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+    ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : false
 })
 
 await client.connect()
--- src/server.ts
+++ src/server.ts
@@ -12,5 +12,5 @@ app.use('/text', textRoutes)
 app.use('/ticker', tickerRoutes)
 app.use('/quiz', quizRoutes)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server running on port ${port}`))
